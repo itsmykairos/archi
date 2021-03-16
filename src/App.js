27f76948a52cd8cfd5ec25598bf1b6ed5c7ed5af
Midhun <itsmykairos@gmail.com>
2021-03-16 17:32:15 +0530

    Sample changes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <h1>It's My Kairos</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ⏳Yes it's the right time ⌛️
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          Learn More....
+
       </header>
     </div>
   );
